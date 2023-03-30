e8d6517e3cff443461bd33eec92d1339ab003b1c
chris-ede9 <chris_ede9@outlook.com>
2023-03-30 16:40:03 +0100

    Updated milestone_3 with functions to make the code cleaner as per requested in Task 3

--- milestone_3.py
+++ milestone_3.py
@@ -1,16 +1,24 @@
 import random
 
+def check_guess(guess):
+    guess = guess.lower()
+    if (guess in word.lower()):
+        print(f"Good guess! {guess} is in the word.")
+        return True
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        return False
+
+def ask_for_input():
+    while True:
+        guess = input("Please enter a single letter ")
+        if (len(guess) == 1 and guess.isalpha() == True):
+            if check_guess(guess) == True:
+                break
+        else:
+            print("Invalid letter. Please enter a single alphabetical character")
+
 word_list = ["apples", "blueberries", "grapes", "oranges", "strawberries"]
 word = random.choice(word_list)
 
-while True: 
-    guess = input("Please enter a single letter ")
-
-    if (len(guess) == 1 and guess.isalpha() == True):
-        if (guess.lower() in word.lower()):
-            print(f"Good guess! {guess} is in the word.")
-            break
-        else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
-    else:
-        print("Invalid letter. Please enter a single alphabetical character")
+ask_for_input()
